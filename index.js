d3e20bcbd2a8bab989202db71c3183854c997bd7
Bertrand RENAUDIN <bertrand.renaudin@odigo.com>
2024-02-13 16:27:30 +0100

    fix index.js + port in docker file

--- index.js
+++ index.js
@@ -10,16 +10,19 @@ app.use(express.static('public'));
 app.get('/visits', (req, res) => {
     let visits = 0;
     try {
-        visits = parseInt(fs.readFileSync(VISITS_FILE, 'utf8'));
+        if (fs.existsSync(VISITS_FILE)) {
+            visits = parseInt(fs.readFileSync(VISITS_FILE, 'utf8'));
+        } else {
+            fs.writeFileSync(VISITS_FILE, '0');
+        }
     } catch (err) {
         console.error(err);
     }
-
     visits++;
     fs.writeFileSync(VISITS_FILE, visits.toString());
-
     res.send(`${visits}`);
 });
+
 
 app.listen(PORT, () => {
   console.log(`L'application est disponible sur http://localhost:${PORT}`);
